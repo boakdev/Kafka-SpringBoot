7ae169c284584ac7f28c8615ad44ff358fbeb332
Bruno-Carvalho <brunooakdev@gmail.com>
2023-11-02 16:50:50 -0300

    Initial commit

---
+++ src/test/java/com/boakdev/shopapi/KafkaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boakdev.shopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
